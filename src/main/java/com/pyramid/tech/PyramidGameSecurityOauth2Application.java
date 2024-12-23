8e6412f95b9e2fd9f4e1f4efdd70311b32975c42
suvorov <suvorov22002@gmail.com>
2024-12-23 09:41:13 +0100

    Initial Project

---
+++ src/main/java/com/pyramid/tech/PyramidGameSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.pyramid.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidGameSecurityOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyramidGameSecurityOauth2Application.class, args);
+	}
+
+}
