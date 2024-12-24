ebc5cb535be1d9ac781b8f3ba1a993d8ae93067c
suvorov <suvorov22002@gmail.com>
2024-12-24 13:07:26 +0100

    feat: configuration of API documentation link and cross origin CORS access

---
+++ src/main/java/com/pyramid/tech/core/config/WebConfig.java
@@ -0,0 +1,8 @@
+package com.pyramid.tech.core.config;/**
+* Created by Suvorov Vassilievitch
+* Date: 24/12/2024
+* Time: 12:47
+* Project Name: pyramid-game-security-oauth2
+*/
+public class WebConfig {
+}
